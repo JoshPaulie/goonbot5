957de738c64ab0b3ee531dcfcdf342d7c7ede745
joshpaulie <joshpaullee@gmail.com>
2022-08-05 13:01:07 -0500

    better instance handling

--- goonbot/goonbot.py
+++ goonbot/goonbot.py
@@ -1,11 +1,26 @@
 import discord
-from discord.commands import slash_command
+
+from .cog_management import collect_cogs, load_cogs
 
 
 class GoonBot(discord.Bot):
+    """Goonbot instance"""
+
+    def __init__(self):
+        super().__init__(
+            intents=discord.Intents(members=True, messages=True, guilds=True, bans=True),
+            owner_ids=[177131156028784640],
+            debug_guilds=[510865274594131968],
+            activity=discord.Activity(type=discord.ActivityType.listening, name="Dev 😙"),
+        )
+
+        collect_cogs()
+        load_cogs(self)
+
     async def on_ready(self):
+        """Prints if bot successfully comes online"""
         print(f"{self.user} has logged in")
 
-    @slash_command()
-    async def ping(self, ctx):
-        await ctx.respond("Pong!")
+    async def on_application_command(self, ctx: discord.ApplicationContext):
+        """Prints when commands are used"""
+        print(f"{ctx.author.name} used {ctx.command.qualified_name}")  # type: ignore
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import os
 
-import discord
 from dotenv import load_dotenv
 
 from goonbot import GoonBot
 
 load_dotenv(".env")
-intents = discord.Intents.default()
-intents.members = True
-goonbot = GoonBot(debug_guilds=[510865274594131968], intents=intents)
+goonbot = GoonBot()
 
 
 goonbot.run(os.getenv("DISCORD"))
