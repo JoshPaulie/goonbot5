73ed85ff3ee381307214fac7335930756d36d6a5
joshpaulie <joshpaullee@gmail.com>
2022-08-05 11:14:09 -0500

    intents

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 
+import discord
 from dotenv import load_dotenv
 
 from goonbot import GoonBot
 
 load_dotenv(".env")
-goonbot = GoonBot(debug_guilds=[510865274594131968])
+intents = discord.Intents.default()
+intents.members = True
+goonbot = GoonBot(debug_guilds=[510865274594131968], intents=intents)
+
 
 goonbot.run(os.getenv("DISCORD"))
