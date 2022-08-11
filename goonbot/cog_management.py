ca63f39937bdd2f974a598b0f15236f77148b72e
joshpaulie <joshpaullee@gmail.com>
2022-08-11 17:24:39 -0500

    Docs buff 💪

--- goonbot/cog_management.py
+++ goonbot/cog_management.py
@@ -1,20 +1,23 @@
+from collections.abc import Iterator
 from pathlib import Path
 
+import discord
 
-def collect_cogs():
-    """Recursively checks all python modules in ./cogs/ directory. All files must be valid cogs.
-    Because it checks recursively, cogs can be further organized into directories"""
+
+def collect_cogs() -> Iterator[str]:
+    """
+    Yields files names from cogs/ folders, formatted so py-cord can load them into the bot as extensions.
+    cogs/general.py -> cogs.general
+    """
     files = Path("cogs").rglob("*.py")
     for file in files:
         if "__init__" not in file.name:
             yield file.as_posix()[:-3].replace("/", ".")
 
 
-def load_cogs(bot):
-    """Takes bot instance argument, "loads" collected cogs into it."""
+def load_cogs(bot: discord.Bot):
+    """
+    Hacky way to load cogs into bot without explicitly specifying new cogs.
+    """
     for cog in collect_cogs():
-        # try:
-        #     bot.load_extension(cog)
-        # except Exception as e:
-        #     console.log(f"Failed to load cog {cog}\n{e}")
-        bot.load_extension(cog)
+        bot.load_extension(cog, store=False)
