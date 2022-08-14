abb73ede810571b74ff2edaea20e17c5e214a04e
joshpaulie <joshpaullee@gmail.com>
2022-08-14 11:31:40 -0500

    Rm dotenv req, "init" keys.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 #!.venv/Scripts/python.exe
 """Entry point to bot."""
-import os
-
-from dotenv import load_dotenv
-
 from goonbot import GoonBot
-
-load_dotenv(".env")
+from keys import DISCORD
 
 if __name__ == "__main__":
     goonbot = GoonBot()
     goonbot.console.rule("goonbot5")
     goonbot.console.log("Starting... ⚙")
-    goonbot.run(os.getenv("DISCORD"))
+    goonbot.run(DISCORD)
