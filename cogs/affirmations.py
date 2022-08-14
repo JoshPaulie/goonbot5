2b7e2786ab0e79568378fc0084a6d2797a49f2b7
joshpaulie <joshpaullee@gmail.com>
2022-08-14 08:58:34 -0500

    init affirmations.py

---
+++ cogs/affirmations.py
@@ -0,0 +1,20 @@
+import random
+
+import discord
+from discord.commands import Option, slash_command
+from discord.ext import commands
+
+
+class Affirmations(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command()
+    async def ily(self, ctx: discord.ApplicationContext, name: Option(discord.Member, "@Mention the Goon you love")):  # type: ignore
+        """Tell someone you love them!"""
+        emotes = ["😍", "😘", "🥰", "🤩", "🤗", "❤", "💕", "💞", "🖤"]
+        await ctx.respond(f"{ctx.author.display_name} loves you, {name.display_name}! {random.choice(emotes)}")  # type: ignore
+
+
+def setup(bot):
+    bot.add_cog(Affirmations(bot))
