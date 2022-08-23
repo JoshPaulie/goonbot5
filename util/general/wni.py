95686314151d3aceeaac67999bbfc8e9dcbf1282
joshpaulie <joshpaullee@gmail.com>
2022-08-22 20:57:33 -0500

    init wni.py

---
+++ util/general/wni.py
@@ -0,0 +1,10 @@
+snide_remarks = [
+    "dang and I really like doing that..",
+    "did it get lost in the mail?",
+    "not shocked",
+    "i thought we were friends?",
+    "guess I'll hang out here..alone..",
+    "i'm starting to get the impression you don't actually want to hangout with me",
+    "do you hate me?",
+    "are you mad at me?",
+]
