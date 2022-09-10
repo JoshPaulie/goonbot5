47d15bed8a15fb533e1d2fba6c7a00d1f951e7a9
joshpaulie <joshpaullee@gmail.com>
2022-09-10 13:29:15 -0500

    Made the change a bit more dynamic, refactored

--- cogs/activities.py
+++ cogs/activities.py
@@ -4,11 +4,11 @@ import discord
 from discord.ext import commands, tasks
 from goonbot import GoonBot
 
-LISTENING_TO = [
+LISTENING = [
     "Owl City",
     "Enemy (J.I.D. Verse Only)",
     "your thoughts",
-    "Dunking Darius (10-Hour Loop)",
+    "Dunkin Darius (10H Loop)",
 ]
 
 PLAYING = [
@@ -17,16 +17,24 @@ PLAYING = [
     "wild rift",
     "toontown",
     "you",
-    "rs3",
     "adventure quest",
     "tribal wars",
     "Spore",
+    "Endless Online",
 ]
 
 WATCHING = [
-    "Arcane Season 2",
+    "Arcane: Season 2",
     "Camp Rock!",
     f"Harry Potter Book 7 Part {random.randint(1, 100)}",
+    "'Justin W' Videos",
+    "'Chunk Crunkland' Videos",
+]
+
+goonbot_activities = [
+    discord.Activity(type=discord.ActivityType.playing, name=random.choice(PLAYING)),
+    discord.Activity(type=discord.ActivityType.listening, name=random.choice(LISTENING)),
+    discord.Activity(type=discord.ActivityType.watching, name=random.choice(WATCHING)),
 ]
 
 
@@ -34,21 +42,15 @@ class Activities(commands.Cog, name="Activities"):
     def __init__(self, bot: GoonBot):
         self.bot = bot
         self.set_activity.start()
+        self.bot.activity = random.choice(goonbot_activities)
 
     def cog_unload(self):
         self.set_activity.cancel()
 
-    @tasks.loop(minutes=15.0)
+    @tasks.loop(minutes=15)
     async def set_activity(self):
-        await self.bot.change_presence(
-            activity=random.choice(
-                [
-                    discord.Activity(type=discord.ActivityType.playing, name=random.choice(PLAYING)),
-                    discord.Activity(type=discord.ActivityType.listening, name=random.choice(LISTENING_TO)),
-                    discord.Activity(type=discord.ActivityType.watching, name=random.choice(WATCHING)),
-                ]
-            )
-        )
+        if random.randint(1, 3) == 1:
+            await self.bot.change_presence(activity=random.choice(goonbot_activities))
 
     @set_activity.before_loop
     async def before_set_activity(self):
