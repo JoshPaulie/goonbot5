e4e6d2c553a4e9791c6ac605baca57337d35e2bb
joshpaulie <joshpaullee@gmail.com>
2022-09-19 15:40:22 -0500

    Removed dep SuggestionModal, made new AnnouncementModal

--- util/meta/modals.py
+++ util/meta/modals.py
@@ -3,35 +3,17 @@ import random
 import discord
 
 
-class SuggestionModal(discord.ui.Modal):
-    """Modal used in meta.suggestion"""
-
+class AnnouncementModal(discord.ui.Modal):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        self.add_item(discord.ui.InputText(label="TLDR"))  # Suggestion title
-        self.add_item(
-            discord.ui.InputText(
-                label="Tell me about it... 🍿", style=discord.InputTextStyle.long, required=False
-            )
-        )
+        self.add_item(discord.ui.InputText(label="Announcements", style=discord.InputTextStyle.multiline))
 
     async def callback(self, interaction: discord.Interaction):
-        sassy_responses = [
-            "Wow! this will definitely be implemented",
-            "Oh, nice idea bu-ddy!",
-            "I'll think about that!",
-        ]
-        embed = discord.Embed(title="Suggestion sent!", description=random.choice(sassy_responses))
-        suggestion_title = self.children[0].value
-        suggestion_details = self.children[1].value or "..."
-        print(f"{interaction.user.display_name} suggests {suggestion_title}: {suggestion_details}")  # type: ignore
-        # await interaction.response.send_message(embed=embed)
         await interaction.response.send_message(
             embed=discord.Embed(
-                title="Manually recorded 📝",
-                description="I have yet to set up a database, so all suggestions are written to a text file.\n \
-                    All suggestions made before the DB will be migrated",
-            ),
-            ephemeral=True,
+                title="📣 Goonbot has been updated!",
+                description=self.children[0].value,
+                color=discord.Color.blurple(),
+            )
         )
