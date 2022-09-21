7808d598b9e690b4f68d9c17562bc2034f1ff5ea
joshpaulie <joshpaullee@gmail.com>
2022-09-21 16:12:11 -0500

    WIP

--- cogs/ecelebs.py
+++ cogs/ecelebs.py
@@ -1,60 +1,116 @@
+from dataclasses import dataclass
+
 import discord
-from discord.commands import slash_command
 from discord.ext import commands
 from goonbot import GoonBot
-from util.ecelebs.views import eCelebView
+from util.ecelebs.views import check_twitch_live, eCelebView, get_latest_youtube_video_url
 
 
-class eCelebs(commands.Cog):
+@dataclass
+class eCeleb:
+    name: str
+    twitch_username: str | None = None
+    youtube_channel_id: str | None = None
+
+
+eCelebs = [
+    eCeleb(
+        "thebausffs",
+        twitch_username="thebausffs",
+        youtube_channel_id="UCu7ODDeIZ4x1rJwM1LCVL8w",
+    ),
+]
+
+
+async def both_socials_callback(ctx: discord.ApplicationContext):
+    await ctx.respond(
+        view=eCelebView(
+            youtube_channel_id=eCeleb.youtube_channel_id,
+            twitch_username=eCeleb.twitch_username,
+        )
+    )
+
+
+async def only_twitch_callback(ctx: discord.ApplicationContext):
+    await ctx.respond(embed=check_twitch_live(eCeleb.twitch_username))  # type: ignore
+
+
+async def only_youtube_callback(ctx: discord.ApplicationContext):
+    await ctx.respond(get_latest_youtube_video_url(eCeleb.youtube_channel_id))  # type: ignore
+
+
+class eCelebsLinkCommands(commands.Cog):
     def __init__(self, bot: GoonBot):
         self.bot = bot
 
-    @slash_command()
-    async def thebausffs(self, ctx: discord.ApplicationContext):
-        await ctx.respond(
-            view=eCelebView(youtube_channel_id="UCu7ODDeIZ4x1rJwM1LCVL8w", twitch_username="thebausffs")
-        )
+        for eCeleb in eCelebs:
+            if all([eCeleb.twitch_username, eCeleb.youtube_channel_id]):
 
-    @slash_command()
-    async def campbell(self, ctx: discord.ApplicationContext):
-        await ctx.respond(view=eCelebView(youtube_channel_id="UCF9IOB2TExg3QIBupFtBDxg"))
+                eCeleb_command = discord.SlashCommand(both_socials_callback)
+                eCeleb_command.name = eCeleb.name
+                eCeleb_command.description = (
+                    f"Check if {eCeleb.name} is live on TTV, or check their latest video on YT!"
+                )
 
-    @slash_command()
-    async def synapse(self, ctx: discord.ApplicationContext):
-        await ctx.respond(view=eCelebView(youtube_channel_id="UCVw8WSz1c_cazwOA0Yk_P_w"))
+            elif eCeleb.twitch_username:
 
-    @slash_command()
-    async def happy_hob(self, ctx: discord.ApplicationContext):
-        await ctx.respond(
-            view=eCelebView(youtube_channel_id="UC0E1n0GRgBW5gR7y7H9TjZQ", twitch_username="the_happy_hob")
-        )
+                eCeleb_command = discord.SlashCommand(only_twitch_callback)
+                eCeleb_command.name = eCeleb.name
+                eCeleb_command.description = f"Check if {eCeleb.name} is live on TTV!"
+            else:
 
-    @slash_command()
-    async def dangheesling(self, ctx: discord.ApplicationContext):
-        await ctx.respond(
-            view=eCelebView(youtube_channel_id="UCVHtlynIkgJxxXrisVUZlYQ", twitch_username="dangheesling")
-        )
+                eCeleb_command = discord.SlashCommand(only_youtube_callback)
+                eCeleb_command.name = eCeleb.name
+                eCeleb_command.description = f"Check {eCeleb.name}'s latest video on YT!"
 
-    @slash_command()
-    async def northernlion(self, ctx: discord.ApplicationContext):
-        await ctx.respond(
-            view=eCelebView(youtube_channel_id="UC3tNpTOHsTnkmbwztCs30sA", twitch_username="northernlion")
-        )
+            self.bot.add_application_command(eCeleb_command)
 
-    @slash_command()
-    async def squeex(self, ctx: discord.ApplicationContext):
-        await ctx.respond(
-            view=eCelebView(youtube_channel_id="UCSnd_UHkXW7uBpjHz4qIq5Q", twitch_username="squeex")
-        )
+    # @slash_command()
+    # async def thebausffs(self, ctx: discord.ApplicationContext):
+    #     await ctx.respond(
+    #         view=eCelebView(youtube_channel_id="UCu7ODDeIZ4x1rJwM1LCVL8w", twitch_username="thebausffs")
+    #     )
 
-    @slash_command()
-    async def fascinating_horror(self, ctx: discord.ApplicationContext):
-        await ctx.respond(view=eCelebView(youtube_channel_id="UCFXad0mx4WxY1fXdbvtg0CQ"))
+    # @slash_command()
+    # async def campbell(self, ctx: discord.ApplicationContext):
+    #     await ctx.respond(view=eCelebView(youtube_channel_id="UCF9IOB2TExg3QIBupFtBDxg"))
 
-    @slash_command()
-    async def settledrs(self, ctx: discord.ApplicationContext):
-        await ctx.respond(view=eCelebView(youtube_channel_id="UCs-w7E2HZWwXmjt9RTvBB_A"))
+    # @slash_command()
+    # async def synapse(self, ctx: discord.ApplicationContext):
+    #     await ctx.respond(view=eCelebView(youtube_channel_id="UCVw8WSz1c_cazwOA0Yk_P_w"))
+
+    # @slash_command()
+    # async def happy_hob(self, ctx: discord.ApplicationContext):
+    #     await ctx.respond(
+    #         view=eCelebView(youtube_channel_id="UC0E1n0GRgBW5gR7y7H9TjZQ", twitch_username="the_happy_hob")
+    #     )
+
+    # @slash_command()
+    # async def dangheesling(self, ctx: discord.ApplicationContext):
+    #     await ctx.respond(
+    #         view=eCelebView(youtube_channel_id="UCVHtlynIkgJxxXrisVUZlYQ", twitch_username="dangheesling")
+    #     )
+
+    # @slash_command()
+    # async def northernlion(self, ctx: discord.ApplicationContext):
+    #     await ctx.respond(
+    #         view=eCelebView(youtube_channel_id="UC3tNpTOHsTnkmbwztCs30sA", twitch_username="northernlion")
+    #     )
+
+    # @slash_command()
+    # async def squeex(self, ctx: discord.ApplicationContext):
+    #     await ctx.respond(
+    #         view=eCelebView(youtube_channel_id="UCSnd_UHkXW7uBpjHz4qIq5Q", twitch_username="squeex")
+    #     )
+
+    # @slash_command()
+    # async def fascinating_horror(self, ctx: discord.ApplicationContext):
+    #     await ctx.respond(view=eCelebView(youtube_channel_id="UCFXad0mx4WxY1fXdbvtg0CQ"))
+
+    # @slash_command()
+    # async def settledrs(self, ctx: discord.ApplicationContext):
+    #     await ctx.respond(view=eCelebView(youtube_channel_id="UCs-w7E2HZWwXmjt9RTvBB_A"))
 
 
 def setup(bot):
-    bot.add_cog(eCelebs(bot))
+    bot.add_cog(eCelebsLinkCommands(bot))
--- util/ecelebs/views.py
+++ util/ecelebs/views.py
@@ -4,6 +4,57 @@ import pyyoutube
 import twitch
 from keys import GOOGLE, TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET
 
+"""
+
+TODO / BRAINSTORMING
+
+- Add eceleb commands programagically ✨
+- Make dataclass that holds eceleb name and social handle(s)
+- Loop over list of these 👆 dataclasses, adding them each as a new command
+- Each command writes its own description based on what socials are passed
+- If only 1/2 of the socials are provided, send the one instead of view
+
+"""
+
+
+def check_twitch_live(twitch_username: str) -> discord.Embed:
+    ttv = twitch.Helix(TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET)
+    eCeleb_twitch = ttv.user(self.twitch_username)  # type: ignore
+
+    twitch_embed = discord.Embed()
+
+    if eCeleb_twitch is None:
+        twitch_embed.title = f"{twitch_username} could not be found."
+        twitch_embed.description = "_Were they banned? Or a name change?_"
+        twitch_embed.color = discord.Color.brand_red()
+        return twitch_embed
+
+    if eCeleb_twitch.is_live:
+        twitch_embed.url = f"https://www.twitch.tv/{twitch_username}"
+        twitch_embed.title = f"{twitch_username} is live!"
+        twitch_embed.description = eCeleb_twitch.stream.title
+        twitch_embed.set_footer(text=f"Began {arrow.get(eCeleb_twitch.stream.started_at).humanize()}")
+        twitch_embed.set_thumbnail(url=eCeleb_twitch.profile_image_url)
+        twitch_embed.color = discord.Color.brand_green()
+    else:
+        twitch_embed.title = f"{eCeleb_twitch.display_name} is offline 😌"
+        twitch_embed.set_thumbnail(url=eCeleb_twitch.offline_image_url)
+        twitch_embed.color = discord.Color.light_grey()
+
+    return twitch_embed
+
+
+def get_latest_youtube_video_url(youtube_channel_id: str) -> str:
+    yt = pyyoutube.Api(api_key=GOOGLE)
+
+    channel_info = yt.get_channel_info(channel_id=youtube_channel_id)
+    uploads_playlist_id = channel_info.items[0].contentDetails.relatedPlaylists.uploads  # type: ignore
+    playlist_items = yt.get_playlist_items(playlist_id=uploads_playlist_id, count=1)  # type: ignore
+    lastest_upload_id = playlist_items.items[0].contentDetails.videoId  # type: ignore
+    lastest_upload_url = f"https://www.youtube.com/watch?v={lastest_upload_id}"
+
+    return lastest_upload_url
+
 
 class eCelebView(discord.ui.View):
     def __init__(
@@ -27,43 +78,13 @@ class eCelebView(discord.ui.View):
             self.add_item(youtube_button)
 
     async def twitch_button_callback(self, interaction: discord.Interaction):
-        ttv = twitch.Helix(TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET)
-        eCeleb_twitch = ttv.user(self.twitch_username)  # type: ignore
-
-        twitch_embed = discord.Embed()
-
-        if eCeleb_twitch is None:
-            await interaction.message.edit(  # type: ignore
-                embed=discord.Embed(
-                    title=f"{self.twitch_username} could not be found.",
-                    description="_Were they banned? Or a name change?_",
-                    color=discord.Color.brand_red(),
-                ),
-                view=None,
-            )
-            return
-
-        if eCeleb_twitch.is_live:
-            twitch_embed.url = f"https://www.twitch.tv/{self.twitch_username}"
-            twitch_embed.title = f"{self.twitch_username} is live!"
-            twitch_embed.description = eCeleb_twitch.stream.title
-            twitch_embed.set_footer(text=f"Began {arrow.get(eCeleb_twitch.stream.started_at).humanize()}")
-            twitch_embed.set_thumbnail(url=eCeleb_twitch.profile_image_url)
-            twitch_embed.color = discord.Color.brand_green()
-        else:
-            twitch_embed.title = f"{eCeleb_twitch.display_name} is offline 😌"
-            twitch_embed.set_thumbnail(url=eCeleb_twitch.offline_image_url)
-            twitch_embed.color = discord.Color.light_grey()
-
-        await interaction.message.edit(embed=twitch_embed, view=None)  # type: ignore
+        if self.twitch_username:
+            twitch_embed = check_twitch_live(self.twitch_username)
+            # ? This cannot be interaction.edit_original_message(). Not sure why, don't care right now
+            await interaction.message.edit(content=twitch_embed, view=None)  # type: ignore
 
     async def youtube_button_callback(self, interaction: discord.Interaction):
-        yt = pyyoutube.Api(api_key=GOOGLE)
-
-        channel_info = yt.get_channel_info(channel_id=self.youtube_channel_id)
-        uploads_playlist_id = channel_info.items[0].contentDetails.relatedPlaylists.uploads  # type: ignore
-        playlist_items = yt.get_playlist_items(playlist_id=uploads_playlist_id, count=1)  # type: ignore
-        lastest_upload_id = playlist_items.items[0].contentDetails.videoId  # type: ignore
-        lastest_upload_url = f"https://www.youtube.com/watch?v={lastest_upload_id}"
-
-        await interaction.message.edit(content=lastest_upload_url, view=None)  # type: ignore
+        if self.youtube_channel_id:
+            lastest_upload_url = get_latest_youtube_video_url(self.youtube_channel_id)
+            # ? This cannot be interaction.edit_original_message(). Not sure why, don't care right now
+            await interaction.message.edit(content=lastest_upload_url, view=None)  # type: ignore
